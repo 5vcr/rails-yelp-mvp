1f5e92b0e7ce16b8a4ecd6c85f148e4f1b97cf22
Sven Renon <svenrenon@gmail.com>
2017-03-22 21:31:35 +0100

    build  2-model Rails app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Restaurant.delete_all
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+40.times do
+  resto = Restaurant.new(
+    name: "The #{Faker::Commerce.color.capitalize}",
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: category.sample
+    )
+  resto.save!
+end
